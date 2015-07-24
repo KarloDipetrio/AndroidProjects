9953ce02dec5c1cf8cfa1b0ad9dd509a00448296
KarloDIpetrio <karlo.dipetrio@gmail.com>
2015-07-24 20:27:05 +0300

    added custom styles

--- ShopingList/app/src/main/java/com/karlodipetrio/shopinglist/gui/MainActivity.java
+++ ShopingList/app/src/main/java/com/karlodipetrio/shopinglist/gui/MainActivity.java
@@ -1,9 +1,6 @@
 package com.karlodipetrio.shopinglist.gui;
 
 import android.app.ActionBar;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
---
+++ ShopingList/app/src/main/java/com/karlodipetrio/shopinglist/gui/TabsPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.karlodipetrio.shopinglist.gui;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * Created by Pavel on 23.07.2015.
+ */
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+        switch(index) {
+            case 0: // return purchases tab
+                return new PurchasesTabFragment();
+
+            case 1: // return basket tab
+                return new BasketTabFragment();
+
+            case 2: // return my saved lists tab
+                return new MyListTabFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // return num of tabs
+        return 3;
+    }
+}
